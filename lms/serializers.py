d0ad241afe7b059c8fc6393c55eb6628985a79f4
Tem Temich (Deckop) <khxkxmxv@yandex.ru>
2026-03-11 16:05:33 +0300

    30.2 Сериализаторы

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,14 +3,30 @@ from .models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    """Полный сериализатор урока для отдельных эндпоинтов."""
+
     class Meta:
         model = Lesson
         fields = "__all__"
 
 
+class CourseLessonSerializer(serializers.ModelSerializer):
+    """Урезанный сериализатор урока для вложенного вывода в курсе."""
+
+    class Meta:
+        model = Lesson
+        fields = (
+            "id",
+            "title",
+            "description",
+            "preview",
+            "video_url",
+        )
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(many=True, read_only=True)
+    lessons = CourseLessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
